34b20841a43bcdcccd4d9e0ec52b294a63d4ee86
VPuosk <vesa.puoskari@gmail.com>
2021-10-13 07:00:07 +0300

    Osa 10-7

--- rate-repository/src/components/AppBar.jsx
+++ rate-repository/src/components/AppBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Pressable } from 'react-native';
+import { View, StyleSheet, Pressable, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
 import Text from './Text';
@@ -24,20 +24,22 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Pressable>
-        <Link to="/">
-          <Text style={styles.textbox}>
-            Repositories
-          </Text>
-        </Link>
-      </Pressable>
-      <Pressable>
-        <Link to="/SignIn">
-          <Text style={styles.textbox}>
-            Sign in
-          </Text>
-        </Link>
-      </Pressable>
+      <ScrollView horizontal>
+        <Pressable>
+          <Link to="/">
+            <Text style={styles.textbox}>
+              Repositories
+            </Text>
+          </Link>
+        </Pressable>
+        <Pressable>
+          <Link to="/SignIn">
+            <Text style={styles.textbox}>
+              Sign in
+            </Text>
+          </Link>
+        </Pressable>
+      </ScrollView>
     </View>
   );
 };
